6f91981870f41b017e9f0c71b8a8fcd16b0d9b84
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2024-12-21 00:58:16 +0100

    catalog-service: Use Environment object to inject port

--- src/test/java/iwo/wintech/catalogservice/web/WebTextConfiguration.java
+++ src/test/java/iwo/wintech/catalogservice/web/WebTextConfiguration.java
@@ -1,11 +1,13 @@
 package iwo.wintech.catalogservice.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestClient;
+
+import java.util.Objects;
 
 @TestConfiguration
 @Slf4j
@@ -13,7 +15,8 @@ class WebTextConfiguration {
 
     @Bean
     @Lazy
-    RestClient restClient(@Value("${local.server.port}") final int port) {
+    RestClient restClient(final Environment environment) {
+        final int port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port")));
         log.info("Creating RestClient for port: {}", port);
         return RestClient.builder()
                 .baseUrl("http://localhost:" + port)
