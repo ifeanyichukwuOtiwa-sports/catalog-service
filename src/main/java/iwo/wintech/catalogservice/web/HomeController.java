68b40751cf571fe861c87146fa793ec1490acbf5
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2024-12-21 08:46:07 +0100

    Add configutayion proprties: Step to Externalization of Configurations

--- src/main/java/iwo/wintech/catalogservice/web/HomeController.java
+++ src/main/java/iwo/wintech/catalogservice/web/HomeController.java
@@ -1,13 +1,17 @@
 package iwo.wintech.catalogservice.web;
 
+import iwo.wintech.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HomeController {
+    private final PolarProperties polarProperties;
 
     @GetMapping("/")
     public String getGreeting() {
-        return "|Welcome To Book Catalog";
+        return polarProperties.getGreeting();
     }
 }
